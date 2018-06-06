8f42b549254b420d4da1cd1d3cf038ec941aa4c0
SuicideLeaf <ljmatulich@gmail.com>
2018-06-06 21:35:59 +1200

    Added BluetoothLE.Droid (avoiding namespace conflict with BluetoothLE.Android and .Android)

---
+++ BluetoothLE.Droid/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+
+namespace BluetoothLE.Droid
+{
+	[Activity( Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true )]
+	public class MainActivity : AppCompatActivity
+	{
+		private Toolbar _toolbar;
+
+		protected override void OnCreate( Bundle savedInstanceState )
+		{
+			base.OnCreate( savedInstanceState );
+
+			SetContentView( Resource.Layout.activity_main );
+
+			FindViews( );
+
+			SetupViews( );
+		}
+
+		public void FindViews( )
+		{
+			_toolbar = FindViewById<Toolbar>( Resource.Id.toolbar );
+		}
+
+		private void SetupViews( )
+		{
+			SetSupportActionBar( _toolbar );
+		}
+	}
+}
+
