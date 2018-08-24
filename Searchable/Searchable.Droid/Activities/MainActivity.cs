8c12a1ddb02248619a3a24b2900f27def0a30197
SuicideLeaf <ljmatulich@gmail.com>
2018-08-24 16:50:20 +1200

    Partial search implementation

---
+++ Searchable/Searchable.Droid/Activities/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Android.Views;
+
+namespace Searchable.Droid.Activities
+{
+	[Activity( Label = "Main", MainLauncher = true )]
+	//[MetaData( "android.app.searchable", Resource = "@xml/searchable" )]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate( Bundle savedInstanceState )
+		{
+			base.OnCreate( savedInstanceState );
+
+			// Set our view from the "main" layout resource
+			SetContentView( Resource.Layout.activity_main );
+		}
+
+//		public override bool OnCreateOptionsMenu( IMenu menu )
+//		{
+//			MenuInflater.Inflate( Resource.Menu.menu_search, menu );
+//
+//			SearchManager searchManager = ( SearchManager ) GetSystemService( SearchService );
+//			SearchView searchView = ( SearchView ) menu.FindItem( Resource.Id.menu_search ).ActionView;
+//
+////			searchView.SetSearchableInfo( searchManager.GetSearchableInfo( ComponentName ) );
+////			searchView.SetIconifiedByDefault( false ); // Do not iconify the widget; expand it by default
+//
+//			return true;
+//		}
+	}
+}
---
+++ Searchable/Searchable.Droid/Activities/SearchableActivity.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Searchable.Droid.Activities
+{
+	[IntentFilter( new[ ] {Intent.ActionSearch} )]
+	[Activity( Label = "Searchable Activity" )]
+	public class SearchableActivity : Activity
+	{
+		protected override async void OnCreate( Bundle savedInstanceState )
+		{
+			base.OnCreate( savedInstanceState );
+			
+			SetContentView( Resource.Layout.activity_search );
+
+			if ( Intent.ActionSearch.Equals( Intent.Action ) )
+			{
+				string query = Intent.GetStringExtra( SearchManager.Query );
+				await DoSearch( query );
+			}
+		}
+
+		private async Task DoSearch( string query )
+		{
+			//throw new System.NotImplementedException( );
+		}
+	}
+}
